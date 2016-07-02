0601e8ddf9acbfe0d2d9f991ea1146319bef3c4c
puhitaku <puhitaku@gmail.com>
2016-07-03 00:01:28 +0900

    Implement fundamental controller

--- server/src/components/Main.jsx
+++ server/src/components/Main.jsx
@@ -1,23 +1,64 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('bootstrap/dist/css/bootstrap.min.css');
 
 import React from 'react';
+import $ from 'jquery';
+import {Button, Grid, Col, Row, MenuItem, Navbar, Nav, NavItem, NavDropdown} from 'react-bootstrap';
 
+class NomatNav extends React.Component {
+  defaultProps = {};
 
-let yeomanImage = require('../images/yeoman.png');
-
-class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href='#'>Nomat Controller</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href='#'>Controller</NavItem>
+            <NavItem eventKey={2} href='#'>Config</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+class NomatController extends React.Component {
+  nomatOn() {
+    console.log('On!');
+    $.ajax({
+      url: './api/on'
+    })
+  }
 
-export default AppComponent;
+  nomatOff() {
+    console.log('Off!');
+    $.ajax({
+      url: './api/off'
+    })
+  }
+
+  render() { return (
+    <div>
+      <NomatNav />
+      <Grid>
+        <Row>
+          <Col xs={12} md={6}>
+            <Button bsSize='large' bsStyle='success' block onClick={this.nomatOn}>ON</Button>
+          </Col>
+          <Col xs={12} md={6}>
+            <Button bsSize='large' bsStyle='danger' block onClick={this.nomatOff}>OFF</Button>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );}
+}
+
+export default NomatController;
